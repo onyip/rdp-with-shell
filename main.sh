eea466e596fa8ccf05cb9ada557488643b081ca1
OGdangID <onyipgtg@gmail.com>
2022-09-23 16:24:40 +0700

    up

--- main.sh
+++ main.sh
@@ -4,8 +4,8 @@ echo 'og:y' | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
-dpkg -i chrome-remote-desktop_current_amd64.deb
-apt install -y --fix-broken
+dpkg --install chrome-remote-desktop_current_amd64.deb
+apt install --assume-yes --fix-broken
 
 export DEBIAN_FRONTEND=noninteractive
 apt install --assume-yes xfce4 desktop-base xfce4-terminal
@@ -15,11 +15,13 @@ apt install --assume-yes xscreensaver
 systemctl disable lightdm.service
 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-dpkg -i google-chrome-stable_current_amd64.deb
-apt install -y --fix-broken
+dpkg -install google-chrome-stable_current_amd64.deb
+apt install --assume-yes --fix-broken
+
+
 adduser og chrome-remote-desktop
 read -p "Enter CRP: " CRP
-bash -c "su - og -c '${CRP} --pin=123456'"
+bash -c 'sudo ${CRP} --pin=123456'
 service chrome-remote-desktop start
 echo "ready to use!"
 nohup sleep 99999999
