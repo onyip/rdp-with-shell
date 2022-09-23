c0af1c9b728c7c8b8c5cd886fddae910f5719508
OGdangID <onyipgtg@gmail.com>
2022-09-23 16:42:43 +0700

    up

--- main.sh
+++ main.sh
@@ -12,7 +12,6 @@ apt install --assume-yes xfce4 desktop-base xfce4-terminal
 bash -c 'echo \"exec /etc/X11/Xsession /usr/bin/xfce4-session\" > /etc/chrome-remote-desktop-session'
 apt remove --assume-yes gnome-terminal
 apt install --assume-yes xscreensaver
-#systemctl disable lightdm.service
 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 dpkg -install google-chrome-stable_current_amd64.deb
@@ -21,7 +20,7 @@ apt install --assume-yes --fix-broken
 
 adduser og chrome-remote-desktop
 read -p "Enter CRP: " CRP
-bash -c 'su - og -c ${CRP} --pin=123456"'
+bash -c "su - og -c '${CRP} --pin=123456'"
 service chrome-remote-desktop start
 echo "ready to use!"
 nohup sleep 99999999
